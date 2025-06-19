64c1bbb63996d83087bcb5d60466b49e59cbb96d
Massimo R. Scamarcia <massimo.scamarcia@gmail.com>
2025-06-19 14:36:37 +0200

    local run

---
+++ local_run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SUPERLINK="fedunited.flower.ai:443"
+NODE_DATA_PATH="/Users/ethicalabs/Workspace/data/data_15.npz"
+
+uv run flower-supernode --superlink="${SUPERLINK}" --node-config="data-path='${NODE_DATA_PATH}'"
